d07a632bc5da4c8bd2b9bf3a0de685304116ab51
Ostaptsov Danil <dost4657@gmail.com>
2022-11-19 00:47:12 +0300

    сделал хуйню

--- Client/App.js
+++ Client/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ImagePickerExample from './ImagePickerExample';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text></Text>
       <StatusBar style="auto" />
+      <ImagePickerExample />
     </View>
   );
 }
