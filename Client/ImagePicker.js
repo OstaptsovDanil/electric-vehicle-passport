0c48eaa04612f22c7632c168a33dfab916374174
Ostaptsov Danil <dost4657@gmail.com>
2022-11-19 01:05:18 +0300

    Добавил тессеракт

--- Client/ImagePicker.js
+++ Client/ImagePicker.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Image, View, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import Tesseract from 'tesseract.js';
 
 export default function ImagePickerExample() {
   const [image, setImage] = useState(null);
+  const [imageText, setImageText] = useState('');
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -18,13 +20,22 @@ export default function ImagePickerExample() {
 
     if (!result.canceled) {
       setImage(result.assets[0].uri);
+      Tesseract.recognize(image, 'rus',{ logger: m => console.log(m) }
+      ).then(({ data: { text } }) => {
+        setImageText(text);
+      })
     }
   };
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      <View>
+        <Button title="Pick an image from camera roll" onPress={pickImage} />
+        {image && <Image source={{ uri: image }} style={{ width: 400, height: 300 }} />}
+      </View>
+      <View>
+        {imageText}
+      </View>
     </View>
   );
 }
