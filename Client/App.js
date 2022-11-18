2e911e36cc89b0e79119abc30586e71114a9c1a0
Ostaptsov Danil <dost4657@gmail.com>
2022-11-18 17:13:48 +0300

    Добавил ImagePicker из документации

--- Client/App.js
+++ Client/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ImagePickerExample from './ImagePicker';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text></Text>
+      <ImagePickerExample />
       <StatusBar style="auto" />
     </View>
   );
