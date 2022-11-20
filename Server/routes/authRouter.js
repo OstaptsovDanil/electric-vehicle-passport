f814be41ce37c377e6baf6dcafed9d6c35113c26
energy-spectrum <brattsev.shura04@mail.ru>
2022-11-20 03:09:39 +0300

    пофиксил конфликты

--- Server/routes/authRouter.js
+++ Server/routes/authRouter.js
@@ -7,8 +7,4 @@ export const authRouter = new Router()
 
 authRouter.post('/registration', registrationValidations, checkValidation, authController.register)
 authRouter.post('/login', authController.login)
-<<<<<<< HEAD
-authRouter.get('/me',getUserId, authController.getUser)
-=======
 authRouter.get('/me', getUserId, authController.getUser)
->>>>>>> eb39762 (Реализована обработка изображения и распознование текста)
